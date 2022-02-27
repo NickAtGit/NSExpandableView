ad37d3a73d2e1f0d3e68a16d3a3cfa91d48cf688
nst <iphone5001@gmail.com>
2022-02-27 14:09:55 +0100

    background color can be set from outside, default corner style is continuous

--- Sources/NSExpandableView/NSExpandableView.swift
+++ Sources/NSExpandableView/NSExpandableView.swift
@@ -7,24 +7,27 @@ public struct NSExpandableView<TopContent: View, BottomContent: View>: View {
     private let cornerRadius: CGFloat
     private let roundedCornerStyle: RoundedCornerStyle
     private let shouldCollapseOnBottomTap: Bool
+    private let backgroundColor: Color
     @State private var isExpanded = false
     
     public init(@ViewBuilder topContent: @escaping () -> TopContent,
                 @ViewBuilder bottomContent: @escaping () -> BottomContent,
                 cornerRadius: CGFloat = 10,
-                roundedCornerStyle: RoundedCornerStyle = .circular,
-                shouldCollapseOnBottomTap: Bool = true) {
+                roundedCornerStyle: RoundedCornerStyle = .continuous,
+                shouldCollapseOnBottomTap: Bool = true,
+                backgroundColor: Color = Color(.secondarySystemBackground)) {
         self.topContent = topContent
         self.bottomContent = bottomContent
         self.cornerRadius = cornerRadius
         self.roundedCornerStyle = roundedCornerStyle
         self.shouldCollapseOnBottomTap = shouldCollapseOnBottomTap
+        self.backgroundColor = backgroundColor
     }
     
     public var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: cornerRadius, style: roundedCornerStyle)
-                .foregroundColor(Color(.secondarySystemBackground))
+                .foregroundColor(backgroundColor)
             VStack(alignment: .leading) {
                 HStack {
                     topContent()
